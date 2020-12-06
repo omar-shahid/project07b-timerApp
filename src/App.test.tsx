92d221537ec82b508707819b291b7435e5dbe51a
ReactReduxRouterDOM <omarshahid160@gmail.com>
2020-12-06 17:12:22 +0500

    Created tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import { render } from "@testing-library/react"
+import App from "./App"
+import { renderMonth, renderWeekDay, runClock } from "./utils/clockFunctions"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("Renders Current Year in Page", () => {
+	const { getByText } = render(<App />)
+	// const linkElement = screen.getByText(//i)
+	const currentYear = getByText(new Date().getFullYear())
+	expect(currentYear).toBeInTheDocument()
+})
+
+test("Timer is rerendering per second", () => {
+	const { getByText } = render(<App />)
+	const currentTime = getByText(runClock())
+	expect(currentTime).toBeInTheDocument()
+	setTimeout(() => {
+		expect(getByText(runClock())).toBeInTheDocument()
+	}, 3000)
+})
+
+test("Bottom text is getting displayed correctly", () => {
+	const { getByText } = render(<App />)
+	const currBottomLine = getByText(
+		`${renderWeekDay()}, ${renderMonth()} ${new Date().getDate()}`
+	)
+	expect(currBottomLine).toBeInTheDocument()
+})
